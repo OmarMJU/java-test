ebc6fb66f93a538fe4e106ce77c1bbeed821080b
Omar M Juarez <omarmju906p@hotmail.com>
2022-04-15 21:01:51 -0500

    Create constant class for movies genre.

---
+++ src/main/java/com/omju/movies/model/ConstantsMovies.java
@@ -0,0 +1,8 @@
+package com.omju.movies.model;
+
+public class ConstantsMovies {
+    public static final String THRILLER = "THRILLER";
+    public static final String ACTION = "ACTION";
+    public static final String COMEDY = "COMEDY";
+    public static final String HORROR = "HORROR";
+}
